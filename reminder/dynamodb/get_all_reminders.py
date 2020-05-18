f14ce5339a86f45ade2bdee18a457e9c533a8d98
Minifranger <clement.franger@gmail.com>
2020-05-18 20:13:22 +0200

    added cors temporarly

--- reminder/dynamodb/get_all_reminders.py
+++ reminder/dynamodb/get_all_reminders.py
@@ -6,5 +6,5 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
 
-def get_all_reminder(event, context):
+def get_all_reminders(event, context):
     return scan(event, context, table=os.environ['DYNAMODB_TABLE'])
