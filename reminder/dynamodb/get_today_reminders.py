a63a27aba87be28d1446316da720260c67061099
Minifranger <clement.franger@gmail.com>
2020-05-19 15:33:08 +0200

    test today reminder

--- reminder/dynamodb/get_today_reminders.py
+++ reminder/dynamodb/get_today_reminders.py
@@ -1,8 +1,10 @@
 import os
 import logging
+from datetime import date
 
 from boto3.dynamodb.conditions import Attr
 
+from dynamodb.get_all_reminders import get_all_reminders
 from dynamodb.scan import scan
 
 logger = logging.getLogger(__name__)
@@ -12,6 +14,6 @@ logger.setLevel(logging.INFO)
 def get_today_reminders(event, context):
     logger.info('event : {event}'.format(event=event))
 
-    params = {'TableName': os.environ['DYNAMODB_TABLE'],
-              'FilterExpression': Attr('id').eq('test')}
-    return scan(event, context, params=params)
+    params = {'TableName': os.environ['DYNAMODB_TABLE']}
+    days = (date.today() - date(2020, 1, 1)).days
+    return [d for d in scan(event, context, params=params) if days % d.frequency == 0]
